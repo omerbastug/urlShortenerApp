9c07a8bf9b139441da70c00c8fdda932631928d3
omerbastug <bomersami@gmail.com>
2022-09-03 20:51:37 +0200

    endpoints

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from 'cors';
 const app = express();
+import bodyparser from "body-parser";
+import urlExists from 'url-exists-nodejs'
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +21,31 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+
+app.use(bodyparser.urlencoded({extended: true}))
+
+let urlArray = new Array("0 index ignored")
+
+app.post("/api/shorturl/", async (req,res) => {
+  let url = req.body.url;
+  
+  const exists = await urlExists(url);// check if it exists
+  if(!exists) return res.json({error: 'invalid url'});
+
+  let i = urlArray.findIndex((element) => element===url);
+  if(i != -1){
+    res.json({"original_url ": url,"short_url" : i})
+  } else {
+    urlArray.push(url);
+    res.json({"original_url ": url,"short_url" : urlArray.length - 1})
+  }
+})
+
+app.get("/api/shorturl/:shorturl", (req,res)=>{
+  res.redirect(urlArray.at(parseInt(req.params.shorturl)))
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
